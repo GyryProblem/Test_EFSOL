588a9a7adb250acd1222a8f6f8eff0bd9030a478
GyryProblem <49600081+GyryProblem@users.noreply.github.com>
2019-04-14 03:58:22 +0300

    Доделать

--- Calc/Calc/Division.cs
+++ Calc/Calc/Division.cs
@@ -54,11 +54,11 @@ namespace Calc
             int divider = Convert.ToInt32(Arguments.number2); //Записываем делитель
             while (!stop)
             {
-                int length_num1 = Arguments.number1.Length;
+                int length_num1 = Arguments.number1.Length; //
                 int c = 0;
                 if (length_num1 >= length_num2)
                 {
-                    int length_num = length_num2;
+                    int length_num = length_num2; //
                     string str_argument1 = Arguments.number1.Substring(0, length_num); //Обрезаем строку под размер делителя
                     int dividend = Convert.ToInt32(str_argument1);                     
                     if (dividend >= divider) //Если делимое больше или равно делителю
@@ -67,7 +67,6 @@ namespace Calc
                         Arguments.result = Arguments.result + Convert.ToString(c);
                         int ostatok = dividend - c * divider;
                         Arguments.number1 = Arguments.number1.Remove(0, length_num);
-                        // if (ostatok != 0) //Если результат операции не равен 0, то обрезав строку добавим остаток
                         Arguments.number1 = Arguments.number1.Insert(0, Convert.ToString(ostatok));
                         if (put_comma)
                         {
