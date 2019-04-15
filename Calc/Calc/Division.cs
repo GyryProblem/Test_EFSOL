761b3fed2bf12500c372a9970f26c25d8b67e230
GyryProblem <49600081+GyryProblem@users.noreply.github.com>
2019-04-15 15:45:03 +0300

    Рабочее деление (пока цикл не ост.)

--- Calc/Calc/Division.cs
+++ Calc/Calc/Division.cs
@@ -55,176 +55,45 @@ namespace Calc
             int divider = Convert.ToInt32(Arguments.number2);
             //while (!stop)
             //{
-            //    int length_num1 = Arguments.number1.Length;
-            //    if (length_take <= length_num1 + Arguments.ostatok.Length)
+            //    int length_num1 = Arguments.number1.Length; //
+            //    int c = 0;
+            //    if (length_num1 >= length_take)
             //    {
-            //        int i = 0; // Кол-во символов, которые будем рать для деления
-            //        while (Arguments.ostatok.Length != length_take)
+            //        int length_num = length_take; //
+            //        string str_argument1 = Arguments.number1.Substring(0, length_num); //Обрезаем строку под размер делителя
+            //        int dividend = Convert.ToInt32(str_argument1);
+            //        if (dividend >= divider) //Если делимое больше или равно делителю
             //        {
-            //            if (Arguments.number1.Length + Arguments.ostatok.Length < length_take)
-            //            {
-            //                Arguments.number1 = Arguments.number1 + '0';
-            //                if (Arguments.result == null)
-            //                {
-            //                    Arguments.result = "0,";
-            //                    put_comma = true;
-            //                }
-            //                else
-            //                {
-            //                    if (!put_comma)
-            //                    {
-            //                        Arguments.result = Arguments.result + ',';
-            //                        put_comma = true;
-            //                    }
-            //                }
-            //            }
-            //            Arguments.ostatok = Arguments.ostatok + Arguments.number1[i];
-            //            if (i > 0)
-            //            {
-            //                if (Arguments.result != null)
-            //                {
-            //                    Arguments.result = Arguments.result + '0';
-            //                }
-            //            }
-            //            i++;
-            //        }
-            //        int dividend = Convert.ToInt32(Arguments.ostatok);
-            //        if (dividend > divider)
-            //        {
-            //            int c = dividend / divider;
+            //            c = dividend / divider;
             //            Arguments.result = Arguments.result + Convert.ToString(c);
-            //            int ost = dividend - divider * c;
-            //            Arguments.ostatok = Convert.ToString(ost);
-            //            Arguments.number1 = Arguments.number1.Remove(0, i);
-            //        }
-            //        else
-            //        {
-            //            if (i <= length_num1)
-            //            {
-            //                if (Arguments.result != null)
-            //                {
-            //                    Arguments.result = Arguments.result + '0';
-            //                }
-            //                Arguments.ostatok = Arguments.ostatok + Arguments.number1[i];
-            //                dividend = Convert.ToInt32(Arguments.ostatok);
-            //                int c = dividend / divider;
-            //                Arguments.result = Arguments.result + Convert.ToString(c);
-            //                int ost = dividend - divider * c;
-            //                Arguments.ostatok = Convert.ToString(ost);
-            //                Arguments.number1 = Arguments.number1.Remove(0, i + 1);
-            //            }
-            //            else
-            //            {
-            //                Arguments.number1 = Arguments.number1 + '0';
-            //                if (!put_comma)
-            //                {
-            //                    Arguments.result = Arguments.result + ',';
-            //                    put_comma = true;
-            //                }
-            //                if (Arguments.result != null)
-            //                {
-            //                    Arguments.result = Arguments.result + '0';
-            //                }
-            //                Arguments.ostatok = Arguments.ostatok + Arguments.number1[i];
-            //                int c = dividend / divider;
-            //                Arguments.result = Arguments.result + Convert.ToString(c);
-            //                int ost = dividend - divider * c;
-            //                Arguments.ostatok = Convert.ToString(ost);
-            //                Arguments.number1 = Arguments.number1.Remove(0, i + 1);
-            //            }
-            //        }
-            //    }
-            //    else
-            //    {
-            //    }
-            //}
-            //bool start = true;
-            //while (!stop)
-            //{
-            //    string ostatok = "";
-            //    int length_num1 = Arguments.number1.Length;
-            //    if (length_num1 >= length_take) //Если длина делимого больше
-            //    {
-            //        int i = 0;
-            //        if (Arguments.ostatok_length >= length_take)
-            //        {
-            //            while (i != Arguments.ostatok_length + 1)
-            //            {
-            //                ostatok = ostatok + Arguments.number1[i];
-            //                i++;
-            //            }
-            //        }
-            //        else
-            //        {
-            //            while (i != length_take)
-            //            {
-            //                ostatok = ostatok + Arguments.number1[i];
-            //                i++;
-            //            }
-            //        }
-            //        if (!start)
-            //        {
-            //            if (Arguments.ostatok_length + 1 < ostatok.Length)
-            //            {
-            //                int add = ostatok.Length - 1 - Arguments.ostatok_length;
-            //                for (int y = 1; y <= add; y++)
-            //                {
-            //                    Arguments.result = Arguments.result + '0';
-            //                }
-
-            //            }
-            //        }
-            //        //if (Arguments.ostatok_length + 1 < ostatok.Length)
-            //        //{
-            //        //    int add = ostatok.Length - 1 - Arguments.ostatok_length;
-            //        //    if (!start)
-            //        //    {
-            //        //        for (int y = 1; y<=add; y++)
-            //        //        {
-            //        //            Arguments.result = Arguments.result + '0';
-            //        //        }
-            //        //    }
-            //        //}
-            //        int dividend = Convert.ToInt32(ostatok);
-            //        if (dividend >= divider)
-            //        {
-            //            int c = dividend / divider;
-            //            Arguments.result = Arguments.result + Convert.ToString(c);
-            //            int ost = dividend - c * divider;
-            //            ostatok = Convert.ToString(ost);
-            //            Arguments.ostatok_length = Convert.ToInt32(ostatok.Length);
-            //            Arguments.number1 = Arguments.number1.Remove(0, i);
-            //            Arguments.number1 = Arguments.number1.Insert(0, ostatok);
+            //            int ostatok = dividend - c * divider;
+            //            Arguments.number1 = Arguments.number1.Remove(0, length_num);
+            //            Arguments.number1 = Arguments.number1.Insert(0, Convert.ToString(ostatok));
             //            if (put_comma)
             //            {
-            //                Arguments.number1 = Arguments.number1 + '0';
+            //                Arguments.number1 = Arguments.number1 + "0";
             //            }
             //        }
-            //        else
+            //        else  //Если делимое меньше делителя
             //        {
-            //            if (i <= length_num1)
+            //            length_num = length_num + 1;
+            //            if (length_num <= length_num1) //Забираем из строки еще одно число
             //            {
+            //                str_argument1 = Arguments.number1.Substring(0, length_num);
+            //                dividend = Convert.ToInt32(str_argument1);
+            //                c = dividend / divider;
+            //                Arguments.result = Arguments.result + Convert.ToString(c);
+            //                int ostatok = dividend - c * divider;
+            //                Arguments.number1 = Arguments.number1.Remove(0, length_num);
+            //                Arguments.number1 = Arguments.number1.Insert(0, Convert.ToString(ostatok));
             //                if (put_comma)
             //                {
-            //                    Arguments.number1 = Arguments.number1 + '0';
+            //                    Arguments.number1 = Arguments.number1 + "0";
             //                }
-            //                if (!start)
-            //                {
-            //                    Arguments.result = Arguments.result + '0';
-            //                }
-            //                ostatok = ostatok + Arguments.number1[i];
-            //                dividend = Convert.ToInt32(ostatok);
-            //                int c = dividend / divider;
-            //                Arguments.result = Arguments.result + Convert.ToString(c);
-            //                int ost = dividend - c * divider;
-            //                ostatok = Convert.ToString(ost);
-            //                Arguments.ostatok_length = Convert.ToInt32(ostatok.Length);
-            //                Arguments.number1 = Arguments.number1.Remove(0, i+1);
-            //                Arguments.number1 = Arguments.number1.Insert(0, ostatok);
             //            }
-            //            else
+            //            else //Добавляем еще 0 
             //            {
-            //                Arguments.number1 = Arguments.number1 + '0';
+            //                Arguments.number1 = Arguments.number1 + "0";
             //                if (!put_comma)
             //                {
             //                    Arguments.result = Arguments.result + ',';
@@ -237,7 +106,7 @@ namespace Calc
             //            }
             //        }
             //    }
-            //    else //Если длина делимого меньше
+            //    else
             //    {
             //        if (!put_comma)
             //        {
@@ -250,89 +119,95 @@ namespace Calc
             //        }
             //        else
             //        {
+            //            Arguments.result = Arguments.result + "0";
             //        }
             //        Arguments.number1 = Arguments.number1 + "0";
-            //        Arguments.ostatok_length = Convert.ToInt32(Arguments.number1.Length);
             //    }
-            //    start = false;
+            //    if (Arguments.number1 == "00")
+            //    {
+            //        break;
+            //    }
             //}
+            Arguments.ostatok = "0";
+            bool start = true;
             while (!stop)
             {
-                int length_num1 = Arguments.number1.Length; //
-                int c = 0;
-                if (length_num1 >= length_take)
+                int length_num1 = Convert.ToInt32(Arguments.number1.Length);
+                int i = 0;
+                int num_dividend = Convert.ToInt32(Arguments.ostatok);
+                while (num_dividend < divider)
                 {
-                    int length_num = length_take; //
-                    string str_argument1 = Arguments.number1.Substring(0, length_num); //Обрезаем строку под размер делителя
-                    int dividend = Convert.ToInt32(str_argument1);
-                    if (dividend >= divider) //Если делимое больше или равно делителю
+                    if (start)
                     {
-                        c = dividend / divider;
-                        Arguments.result = Arguments.result + Convert.ToString(c);
-                        int ostatok = dividend - c * divider;
-                        Arguments.number1 = Arguments.number1.Remove(0, length_num);
-                        Arguments.number1 = Arguments.number1.Insert(0, Convert.ToString(ostatok));
-                        if (put_comma)
+                        if (Arguments.number1 == "")
                         {
-                            Arguments.number1 = Arguments.number1 + "0";
-                        }
-                    }
-                    else  //Если делимое меньше делителя
-                    {
-                        length_num = length_num + 1;
-                        if (length_num <= length_num1) //Забираем из строки еще одно число
-                        {
-                            str_argument1 = Arguments.number1.Substring(0, length_num);
-                            dividend = Convert.ToInt32(str_argument1);
-                            c = dividend / divider;
-                            Arguments.result = Arguments.result + Convert.ToString(c);
-                            int ostatok = dividend - c * divider;
-                            Arguments.number1 = Arguments.number1.Remove(0, length_num);
-                            Arguments.number1 = Arguments.number1.Insert(0, Convert.ToString(ostatok));
-                            if (put_comma)
-                            {
-                                Arguments.number1 = Arguments.number1 + "0";
-                            }
-                        }
-                        else //Добавляем еще 0 
-                        {
-                            Arguments.number1 = Arguments.number1 + "0";
                             if (!put_comma)
                             {
-                                Arguments.result = Arguments.result + ',';
+                                Arguments.result = Arguments.result + "0,";
+                                Arguments.number1 = Arguments.number1 + '0';
                                 put_comma = true;
                             }
                             else
                             {
-                                Arguments.result = Arguments.result + "0";
+                                Arguments.result = Arguments.result + '0';
+                                Arguments.number1 = Arguments.number1 + '0';
                             }
                         }
-                    }
-                }
-                else
-                {
-                    if (!put_comma)
-                    {
-                        if (Arguments.result == null)
-                        {
-                            Arguments.result = Arguments.result + "0";
-                        }
-                        Arguments.result = Arguments.result + ",";
-                        put_comma = true;
+                        Arguments.ostatok = Arguments.ostatok + Arguments.number1[0];
+                        Arguments.number1 = Arguments.number1.Remove(0, 1);
+                        num_dividend = Convert.ToInt32(Arguments.ostatok);
                     }
                     else
                     {
-                        Arguments.result = Arguments.result + "0";
+                        if (i == 0)
+                        {
+                            if (Arguments.number1 == "")
+                            {
+                                if (!put_comma)
+                                {
+                                    Arguments.result = Arguments.result + ',';
+                                    Arguments.number1 = Arguments.number1 + '0';
+                                    put_comma = true;
+                                }
+                                else
+                                {
+                                    Arguments.number1 = Arguments.number1 + '0';
+                                }
+                            }
+                            Arguments.ostatok = Arguments.ostatok + Arguments.number1[0];
+                            Arguments.number1 = Arguments.number1.Remove(0, 1); //Т.к. забрали элемент в деление, удаляем его
+                            num_dividend = Convert.ToInt32(Arguments.ostatok);
+                        }
+                        else
+                        {
+                            Arguments.result = Arguments.result + '0'; // Элементов было взято больше чем 1, поэтому в результат добавляем 0
+                            if (Arguments.number1 == "")
+                            {
+                                if (!put_comma)
+                                {
+                                    Arguments.result = Arguments.result + ',';
+                                    Arguments.number1 = Arguments.number1 + '0';
+                                    put_comma = true;
+                                }
+                                else
+                                {
+                                    Arguments.number1 = Arguments.number1 + '0';
+                                }
+                            }
+                            Arguments.ostatok = Arguments.ostatok + Arguments.number1[0];
+                            Arguments.number1 = Arguments.number1.Remove(0, 1);
+                            num_dividend = Convert.ToInt32(Arguments.ostatok);
+                        }
+                        i++;
                     }
-                    Arguments.number1 = Arguments.number1 + "0";
                 }
-                if (Arguments.number1 == "00")
-                {
-                    break;
-                }
+                num_dividend = Convert.ToInt32(Arguments.ostatok);
+                int c = num_dividend / divider;
+                Arguments.result = Arguments.result + Convert.ToString(c);
+                int ost = num_dividend - c * divider;
+                Arguments.ostatok = Convert.ToString(ost);
+                start = false;
             }
         }
     }
 }
-/* Новый алгоритм для деления: в самом начале цикла забираем длину числа такую, чтобы оно делилось нацело,
- * если число меньшей длины чем сам делитель и первая операция
